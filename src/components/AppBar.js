881a98af589472e0bfc68ee444ee6b004cae9842
sa-webb <sawebb30@gmail.com>
2020-05-14 15:15:29 -0400

    initial

---
+++ src/components/AppBar.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
+import Badge from '@material-ui/core/Badge'
+
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+
+import { useShoppingCart } from 'use-shopping-cart'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
+
+const Header = ({ siteTitle }) => {
+  const classes = useStyles()
+  const {
+    cartCount,
+  } = useShoppingCart()
+  return (
+    <div className={classes.root}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          News
+        </Typography>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+          <Badge badgeContent={cartCount} color="secondary">
+            <ShoppingCartIcon />
+          </Badge>
+        </IconButton>{' '}
+      </Toolbar>
+    </AppBar>
+  </div>
+  )
+}
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
