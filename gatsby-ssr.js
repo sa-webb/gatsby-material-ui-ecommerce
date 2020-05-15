3451630dbc6dac6fb41b170650d2fb50502cb23b
sa-webb <sawebb30@gmail.com>
2020-05-15 14:02:57 -0400

    CartProvider

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,20 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react'
+import { loadStripe } from '@stripe/stripe-js'
+import { CartProvider } from 'use-shopping-cart'
 
-// You can delete this file if you're not using it
+const stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <CartProvider
+      stripe={stripePromise}
+      successUrl={`${window.location.origin}/page-2/`}
+      cancelUrl={`${window.location.origin}/`}
+      currency="USD"
+      allowedCountries={['US', 'GB', 'CA']}
+      billingAddressCollection={true}
+    >
+      {element}
+    </CartProvider>
+  )
+}
