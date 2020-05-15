19f1dac9f2f8d3cc782c3023f20e2b07e16d1c7b
sa-webb <sawebb30@gmail.com>
2020-05-15 15:33:13 -0400

    example context

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,19 +2,23 @@ import React from 'react'
 import { loadStripe } from '@stripe/stripe-js'
 import { CartProvider } from 'use-shopping-cart'
 
+import { CountProvider } from './src/global/context'
+
 const stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
 
 export const wrapRootElement = ({ element }) => {
   return (
-    <CartProvider
-      stripe={stripePromise}
-      successUrl={`${window.location.origin}/page-2/`}
-      cancelUrl={`${window.location.origin}/`}
-      currency="USD"
-      allowedCountries={['US', 'GB', 'CA']}
-      billingAddressCollection={true}
-    >
-      {element}
-    </CartProvider>
+    <CountProvider>
+      <CartProvider
+        stripe={stripePromise}
+        successUrl={`${window.location.origin}/page-2/`}
+        cancelUrl={`${window.location.origin}/`}
+        currency="USD"
+        allowedCountries={['US', 'GB', 'CA']}
+        billingAddressCollection={true}
+      >
+        {element}
+      </CartProvider>
+    </CountProvider>
   )
 }
