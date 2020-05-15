f9e3b1b105b2ce17e1f856c543c318f82abf381a
sa-webb <sawebb30@gmail.com>
2020-05-15 14:31:16 -0400

    removed provider

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,24 +4,12 @@ import Skus from '../components/Products/Skus'
 import CartOverview from '../components/CartOverview'
 import Header from '../components/AppBar'
 
-import { loadStripe } from '@stripe/stripe-js'
-import { CartProvider } from 'use-shopping-cart'
-
-const stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
-
 const Index = () => (
-    <CartProvider
-      stripe={stripePromise}
-      successUrl={`${window.location.origin}/page-2/`}
-      cancelUrl={`${window.location.origin}/`}
-      currency="USD"
-      allowedCountries={['US', 'GB', 'CA']}
-      billingAddressCollection={true}
-    >
-      <Header />
-      <CartOverview />
-      <Skus />
-    </CartProvider>
+  <>
+    <Header />
+    <CartOverview />
+    <Skus />
+  </>
 )
 
 export default Index
